93057a7237977e1f14638782cfdb5ead93783d05
AVS1508 <aditya.11.present@gmail.com>
2020-05-13 11:42:56 +0530

    Updates

---
+++ precache-manifest.553d07ccc1b1a760c26ad8bcf0ad3cc2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d1c71c3933a46411ed83127ed9fcba4",
+    "url": "/space-gallery-app/index.html"
+  },
+  {
+    "revision": "ce4574ab8d2a4aa1c7ad",
+    "url": "/space-gallery-app/static/css/main.0b220361.chunk.css"
+  },
+  {
+    "revision": "530be5d041126ab56bb9",
+    "url": "/space-gallery-app/static/js/2.5870fe08.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/space-gallery-app/static/js/2.5870fe08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce4574ab8d2a4aa1c7ad",
+    "url": "/space-gallery-app/static/js/main.48aa0897.chunk.js"
+  },
+  {
+    "revision": "ed07a0624633f3a73f5b",
+    "url": "/space-gallery-app/static/js/runtime-main.6f8fbeac.js"
+  },
+  {
+    "revision": "b45f202be9577c660a83c8eb9fc2656c",
+    "url": "/space-gallery-app/static/media/darkrift-space.b45f202b.jpg"
+  }
+]);
