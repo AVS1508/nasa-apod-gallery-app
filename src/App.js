5ddf3c186a79f6f22a0fdd161932122f45224206
AVS1508 <aditya.11.present@gmail.com>
2022-05-09 22:00:27 -0400

    Updated the layout and converted to functional components

--- src/App.js
+++ src/App.js
@@ -1,42 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { fetchData } from "./api";
 
 import styles from "./App.module.css"
 
-class App extends React.Component {
+const App = () => {
+    const [title, setTitle] = useState(undefined);
+    const [url, setUrl] = useState(undefined);
+    const [explanation, setExplanation] = useState(undefined);
 
-    state = {
-        data: {},
-    }
+    useEffect(() => {
+        fetchData()
+            .then(
+                (result) => {
+                    setTitle(result?.data?.title);
+                    setUrl(result?.data?.url);
+                    setExplanation(result?.data?.explanation);
+                })
+            .catch(
+                (error) => {
+                    setTitle("Internal Server Error");
+                    setUrl("");
+                    setExplanation("The web application is facing some issues, currently! Please return at another time!");
+                    console.log(error);
+                });
+    });
 
-    async componentDidMount() {
-        const fetchedData = await fetchData();
-        if (fetchedData.data !== undefined) {
-            this.setState({ data: fetchedData.data });
-        } else {
-            return false;
-        }
-    }
-
-    render() {
-        const { data: { date, explanation, title, url } } = this.state;
-        return (
-            <div className={styles.containerOuter}>
-                <div className={styles.containerInner}>
-                    <div className={styles.header}>
-                        <h1>NASA Astronomy Picture of the Day for {new Date(date).toDateString()}</h1>
-                    </div>
-                </div>
-                <div className={styles.containerInner}>
-                    <img className={styles.image} src={url} alt={title} />
-                    <div className={styles.text}>
-                        <h1>{title}</h1>
-                        <p>{explanation}</p>
-                    </div>
+    return (
+        <div className={styles.containerOuter}>
+            <div className={styles.containerInner}>
+                <div className={styles.header}>
+                    <h1>NASA Astronomy Picture of the Day</h1>
                 </div>
             </div>
-        );
-    }
+            <div className={styles.containerInner}>
+                {/* <img className={styles.image} src={url} alt={title} /> */}
+                <video className={styles.image} src={url} alt={title} controls></video>
+                <div className={styles.text}>
+                    <h1>{title}</h1>
+                    <p>{explanation}</p>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
